b3afd4aa5e8cfcd08d7172a2179dcd21b6b051c0
pipeline <pipeline@syncfusion.com>
2017-11-01 19:40:32 +0000

    config(EJ2-000): api json included (HEAD -> master, origin/master)

--- grid/index-all.ts
+++ grid/index-all.ts
@@ -1,3 +1,3 @@
 import * as index from './index'; 
-index.Grid.Inject( index.Filter, index.Page, index.Selection, index.Sort, index.Group, index.Reorder, index.RowDD, index.DetailRow, index.Toolbar, index.Aggregate, index.Search); 
+index.Grid.Inject( index.Filter, index.Page, index.Selection, index.Sort, index.Group, index.Reorder, index.RowDD, index.DetailRow, index.Toolbar, index.Aggregate, index.Search, index.VirtualScroll, index.Edit, index.Resize, index.ExcelExport, index.PdfExport); 
 export * from './index'; 
