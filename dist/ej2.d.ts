dfbf8c2ad4bbb9fd23e2ff4b3301a24747bcd94d
pipeline <pipeline@syncfusion.com>
2018-02-22 03:27:24 +0000

    v16.1.24-preview is released

---
+++ dist/ej2.d.ts
@@ -0,0 +1,50 @@
+/**
+ * ej2 source
+ */
+import * as _base from '../base';
+import * as _buttons from '../buttons';
+import * as _calendars from '../calendars';
+import * as _cards from '../cards';
+import * as _charts from '../charts';
+import * as _circulargauge from '../circulargauge';
+import * as _compression from '../compression';
+import * as _data from '../data';
+import * as _dropdowns from '../dropdowns';
+import * as _excelexport from '../excel-export';
+import * as _fileutils from '../file-utils';
+import * as _grids from '../grids';
+import * as _index from '../index';
+import * as _inputs from '../inputs';
+import * as _lineargauge from '../lineargauge';
+import * as _lists from '../lists';
+import * as _maps from '../maps';
+import * as _navigations from '../navigations';
+import * as _pdfexport from '../pdf-export';
+import * as _popups from '../popups';
+import * as _schedule from '../schedule';
+import * as _splitbuttons from '../splitbuttons';
+
+export declare namespace ej {
+    const base: typeof _base;
+    const buttons: typeof _buttons;
+    const calendars: typeof _calendars;
+    const cards: typeof _cards;
+    const charts: typeof _charts;
+    const circulargauge: typeof _circulargauge;
+    const compression: typeof _compression;
+    const data: typeof _data;
+    const dropdowns: typeof _dropdowns;
+    const excelexport: typeof _excelexport;
+    const fileutils: typeof _fileutils;
+    const grids: typeof _grids;
+    const index: typeof _index;
+    const inputs: typeof _inputs;
+    const lineargauge: typeof _lineargauge;
+    const lists: typeof _lists;
+    const maps: typeof _maps;
+    const navigations: typeof _navigations;
+    const pdfexport: typeof _pdfexport;
+    const popups: typeof _popups;
+    const schedule: typeof _schedule;
+    const splitbuttons: typeof _splitbuttons;
+}
